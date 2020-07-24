87150c243a0a6eaf069e0cf7d89869c422efaca7
ianbautista <ianfbautista@gmail.com>
2020-07-24 10:41:21 -0700

    added values in form

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -124,7 +124,7 @@ export default function PizzaForm() {
 					/>
 				</div>
 				<h3>Build Your Own Pizza</h3>
-				<form action="">
+				<form onSubmit={formSubmit}>
 					<label htmlFor="name">
 						Name:&nbsp;&nbsp;
 						<input
@@ -133,13 +133,15 @@ export default function PizzaForm() {
 							type="text"
 							placeholder="Your name here"
 							maxLength="40"
+							value={formValues.name}
+							onChange={inputChange}
 						/>
 					</label>
 					<br />
 					<label htmlFor="size">
 						{" "}
 						Choice of Size
-						<select name="size" id="sizeInput">
+						<select name="size" id="sizeInput" value={formValues.size} onChange={inputChange}>
 							<option value="" selected disabled>
 								- select size -
 							</option>
@@ -151,7 +153,7 @@ export default function PizzaForm() {
 					</label>
 					<br />
 					<h4>Choice of Sauce </h4>
-					<input name="sauce" type="radio" value="Original Red" />
+					<input name="sauce" type="radio" value="Original Red" checked="checked" />
 					<label htmlFor="sauce">Original Red</label>
 					<br />
 					<input name="sauce" type="radio" value="Garlic Ranch" />
@@ -165,33 +167,103 @@ export default function PizzaForm() {
 					<br />
 					<div className="toppingsDiv">
 						<h4>Add Toppings</h4>
-						<input name="pepperoni" type="checkbox" />
+						<input
+							name="pepperoni"
+							type="checkbox"
+							checked={formValues.pepperoni}
+							onChange={onCheckboxChange}
+						/>
 						<label htmlFor="pepperoni">Pepperoni</label> <br />
-						<input name="sausage" type="checkbox" />
+						<input
+							name="sausage"
+							type="checkbox"
+							checked={formValues.sausage}
+							onChange={onCheckboxChange}
+						/>
 						<label htmlFor="sausage">Sausage</label> <br />
-						<input name="canadianBacon" type="checkbox" />
+						<input
+							name="canadianBacon"
+							type="checkbox"
+							checked={formValues.canadianBacon}
+							onChange={onCheckboxChange}
+						/>
 						<label htmlFor="canadianBacon">Canadian Bacon</label> <br />
-						<input name="spicyItalianSausage" type="checkbox" />
+						<input
+							name="spicyItalianSausage"
+							type="checkbox"
+							checked={formValues.spicyItalianSausage}
+							onChange={onCheckboxChange}
+						/>
 						<label htmlFor="spicyItalianSausage">Spicy Italian Sausage</label> <br />
-						<input name="grilledChicken" type="checkbox" />
+						<input
+							name="grilledChicken"
+							type="checkbox"
+							checked={formValues.grilledChicken}
+							onChange={onCheckboxChange}
+						/>
 						<label htmlFor="grilledChicken">Grilled Chicken</label> <br />
-						<input name="onions" type="checkbox" />
+						<input
+							name="onions"
+							type="checkbox"
+							checked={formValues.onions}
+							onChange={onCheckboxChange}
+						/>
 						<label htmlFor="onions">Onions</label> <br />
-						<input name="greenPepper" type="checkbox" />
+						<input
+							name="greenPepper"
+							type="checkbox"
+							checked={formValues.greenPepper}
+							onChange={onCheckboxChange}
+						/>
 						<label htmlFor="greenPepper">Green Pepper</label> <br />
-						<input name="dicedTomatos" type="checkbox" />
+						<input
+							name="dicedTomatos"
+							type="checkbox"
+							checked={formValues.dicedTomatos}
+							onChange={onCheckboxChange}
+						/>
 						<label htmlFor="dicedTomatos">Diced Tomatos</label> <br />
-						<input name="blackOlives" type="checkbox" />
+						<input
+							name="blackOlives"
+							type="checkbox"
+							checked={formValues.blackOlives}
+							onChange={onCheckboxChange}
+						/>
 						<label htmlFor="blackOlives">Black Olives</label> <br />
-						<input name="roastedGarlic" type="checkbox" />
+						<input
+							name="roastedGarlic"
+							type="checkbox"
+							checked={formValues.roastedGarlic}
+							onChange={onCheckboxChange}
+						/>
 						<label htmlFor="roastedGarlic">Roasted Garlic</label> <br />
-						<input name="artichokeHearts" type="checkbox" />
+						<input
+							name="artichokeHearts"
+							type="checkbox"
+							checked={formValues.artichokeHearts}
+							onChange={onCheckboxChange}
+						/>
 						<label htmlFor="artichokeHearts">Artichoke Hearts</label> <br />
-						<input name="threeCheese" type="checkbox" />
+						<input
+							name="threeCheese"
+							type="checkbox"
+							checked={formValues.threeCheese}
+							onChange={onCheckboxChange}
+						/>
 						<label htmlFor="threeCheese">Three Cheese</label> <br />
-						<input name="pineapple" type="checkbox" />
+						<input
+							name="pineapple"
+							type="checkbox"
+							checked={formValues.pineapple}
+							onChange={onCheckboxChange}
+						/>
 						<label htmlFor="pineapple">Pineapple</label> <br />
-						<input name="mushroom" type="checkbox" />
+						<input
+							name="mushroom"
+							type="checkbox"
+							checked={formValues.mushroom}
+							onChange={onCheckboxChange}
+						/>
 						<label htmlFor="mushroom">Mushroom</label> <br />
 					</div>
 					<label htmlFor="instructions">
@@ -199,14 +271,18 @@ export default function PizzaForm() {
 						<input
 							id="instructions"
 							name="instructions"
-							type="text"
+							type="textarea"
 							placeholder="Anything else you'd like to add?"
 							maxLength="1000"
+							value={formValues.instructions}
+							onChange={inputChange}
 						/>
-					</label>
+					</label>{" "}
+					<br />
+					<button type="submit" disabled={disabled}>
+						Add to Order
+					</button>
 				</form>
-
-				<button>Add to Order</button>
 			</div>
 		</>
 	);
